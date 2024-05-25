5c84903d53526016500ddea285e528408125a1b3
misha <misha@gmail.com>
2024-05-25 17:36:03 +0300

    added map

--- game.py
+++ game.py
@@ -1,1 +1,15 @@
- 
+from direct.showbase.ShowBase import ShowBase
+import builtins
+from mapmanager import MapManager
+
+
+class Game(ShowBase):
+    def __init__(self):
+        super().__init__()
+        self.land  = MapManager()
+        self.land.loadLand("land.txt")
+        builtins.base.camLens.setFov(90)
+
+
+game = Game()
+game.run()
